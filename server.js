e2b6ecb273af203f526b67ae78ba21e2cfed475f
Darshan <dave1089@live.com>
2016-10-17 22:17:36 +0000

    second commit

--- server.js
+++ server.js
@@ -1,20 +1,34 @@
-// var express = require('express');
-// var app = express();
+var express = require("express")
+var app = express();
+var shortid = require('shortid');
 var port = process.env.PORT || 8080;
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
+app.use(express.static(__dirname + '/public'))
+// app.get('/:path', function (req, res) {
+//   var param = req.params.path
+//   if(isNaN(param)){
+//     var check = Date.parse(param);
+//     if(isNaN(check)){
+//       natural = null
+//       unix = null 
+//     }
+//     else{
+//     unix = new Date(param).getTime()/1000
+//     natural = param
+//     }
+//   }
+//   else {
+//     unix = Number(param)
+//     var change = new Date(param*1000)
+//     var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+//     var y = change.getFullYear();
+//     var m = months[change.getMonth()]
+//     var d = change.getDate()
+//     natural = m + " " + d + "," + " " + y
+//   }
+//   var data = JSON.stringify({"unix" : unix , "natural" : natural})
+//   res.send(data);
 // });
 
-// app.listen(port, function () {
-//   console.log('Example app listening on port ' + port);
-// });
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  var os = req.headers["user-agent"].split('(');
-  var lang = req.headers["accept-language"].substr(0,req.headers["accept-language"].indexOf(","))
-  res.end(JSON.stringify({"OS": os[1].split(')')[0] , "language" :  lang, "ip" : req.headers["x-forwarded-for"]}));
-  // res.end(JSON.stringify(req.headers));
-  
-}).listen(port);
+app.listen(port, function () {
+  console.log('The app listening on port 8080!');
+});
